2e67c7112165f4168d676018f606126467717ecd
josedataismo <jose.campos@dataismo.mx>
2025-06-24 02:22:17 -0600

    Se reestructura la base de datos para evitar redundancia.

---
+++ database/migrations/2025_06_21_103601_create_materias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('materias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre')->unique();
+            $table->integer('nivel');
+            $table->unsignedBigInteger('unidad_id');
+            $table->unsignedBigInteger('usuario_id');
+            $table->timestamps();
+
+            //$table->foreign('unidad_id')->references('id')->on('unidads');
+            $table->foreign('usuario_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('materias');
+    }
+};
