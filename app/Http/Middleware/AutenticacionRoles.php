be78268063735b4213eab299a9c657b5376f444a
josedataismo <jose.campos@dataismo.mx>
2025-06-25 19:35:14 -0600

    Se crea un middleware para autenticación de roles.

---
+++ app/Http/Middleware/AutenticacionRoles.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AutenticacionRoles
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            /** @var User $user */
+            $user = Auth::user();
+            $rolClave = $user->clave;
+            switch ($rolClave){
+                case 'admin': // admin procede a todos lados
+                    return $next($request);
+                    break;
+                case 'otra wea':
+                    return redirect()->route('otra.wea');
+                    default:
+            }
+        }
+
+
+        return $next($request);
+    }
+}
